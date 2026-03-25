df3069d4018138c90d8c2794a937b4d80d61ad58
JuliaPalna <palna.julia@gmail.com>
2026-03-25 04:23:42 +0300

    users-создать reducer,selector

--- client/src/app/providers/reducers/usersReducer/usersInitialState.ts
+++ client/src/app/providers/reducers/usersReducer/usersInitialState.ts
@@ -1,6 +1,3 @@
 import type { UsersStateProps } from '../../../../entities';
 
-export const usersInitialState: UsersStateProps = [
-    { id: 1, name: 'David', registrationAt: 345 },
-    { id: 2, name: 'Lubov', registrationAt: 234 },
-];
+export const usersInitialState: UsersStateProps = [];
