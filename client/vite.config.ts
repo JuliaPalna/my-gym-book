886474f4563b525bf791ba54dafd22040018c565
palnajulia <palna.julia@gmail.com>
2026-07-15 01:43:47 +0300

    refactoring

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -76,9 +76,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                // target: 'http://186.246.6.170:3004',
-                target: 'http://localhost:3004',
-
+                target: 'http://186.246.6.170:3004',
                 changeOrigin: true,
             },
         },
